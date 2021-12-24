d8c5343b5150fcb3eba39c8efd671d555e40ea7e
liveprog <giordanobruno227@gmail.com>
2021-12-24 13:29:13 +0100

    aggiornamento +

--- Es5_1/Translator.java
+++ Es5_1/Translator.java
@@ -16,70 +16,177 @@ public class Translator {
     }
 
     void move() { 
-	// come in Esercizio 3.1
+        look = lex.lexical_scan(pbr);
+        System.out.println("token = " + look);
     }
 
     void error(String s) { 
-	// come in Esercizio 3.1
+        throw new Error("near line " + lex.line + ": " + s);
     }
 
     void match(int t) {
-	// come in Esercizio 3.1
+        if (look.tag == t) {
+            if (look.tag != Tag.EOF) move();
+        } else error("syntax error");
     }
 
-    public void prog() {        
-	// ... completare ...
-        int lnext_prog = code.newLabel();
-        statlist(lnext_prog);
-        code.emitLabel(lnext_prog);
-        match(Tag.EOF);
-        try {
-        	code.toJasmin();
+    public void prog() {    
+        switch(look.tag){    
+            case Tag.ASSIGN:
+            case Tag.PRINT:
+            case Tag.READ:
+            case Tag.WHILE:
+            case Tag.IF:
+            case '{':
+            int lnext_prog = code.newLabel();
+            statlist(lnext_prog);
+            code.emitLabel(lnext_prog);
+            match(Tag.EOF);
+            try {
+                code.toJasmin();
+            }
+            catch(java.io.IOException e) {
+                System.out.println("IO error\n");
+            }
         }
-        catch(java.io.IOException e) {
-        	System.out.println("IO error\n");
-        };
-	// ... completare ...
     }
 
-    public void stat( /* completare */ ) {
+    public void stat() {
         switch(look.tag) {
-	// ... completare ...
+	
             case Tag.READ:
                 match(Tag.READ);
                 match('(');
-	        idlist(/* completare */);
+	            idlist(/* completare */);
                 match(')');
-	// ... completare ...
+                break;
+
+            case Tag.PRINT:
+                match(Tag.PRINT);
+                match('(');
+	            exprlist(/* completare */);
+                match(')');
+                break;
+
+            case Tag.ASSIGN:
+                match(Tag.ASSIGN);
+                expr();
+                match(Tag.TO);
+                idlist(/* completare */);
+                break;
+
+            case Tag.WHILE:
+                match(Tag.WHILE);
+                match('(');
+                bexpr();
+                match(')');
+                stat(); /*probabile etichetta*/
+                break;
+
+                case Tag.IF:
+                match(Tag.IF);
+                match('(');
+                bexpr();
+                match(')');
+                stat();
+                Fprimo(); /*occhio*/
+                break;
+                
+                case '{':
+                match('{');
+                statlist();
+                match('}');
+                break;
+
+            default:
+                error("error in grammar <stat>");
         }
      }
 
-    private void idlist(/* completare */) {
+    private void idlist(String x) {
         switch(look.tag) {
-	    case Tag.ID:
-        	int id_addr = st.lookupAddress(((Word)look).lexeme);
-                if (id_addr==-1) {
-                    id_addr = count;
-                    st.insert(((Word)look).lexeme,count++);
-                }
+            case Tag.ID:
+                int id_addr = st.lookupAddress(((Word)look).lexeme);
+                    if (id_addr==-1) {
+                        id_addr = count;
+                        st.insert(((Word)look).lexeme,count++);
+                    }
                 match(Tag.ID);
-	// ... completare ...
+            default:
+                error("error in grammar <idlist>");
     	}
     }
 
-    private void expr( /* completare */ ) {
+    private void expr() {
         switch(look.tag) {
-	// ... completare ...
+	
             case '-':
                 match('-');
                 expr();
                 expr();
                 code.emit(OpCode.isub);
                 break;
-	// ... completare ...
+
+            case '+':
+                match('+');
+                match('(');
+                exprlist();
+                match(')');
+                code.emit(OpCode.iadd);
+                break;
+
+            case '/':
+                match('/');
+                expr();
+                expr();
+                code.emit(OpCode.idiv);
+                break;
+
+            case '*':
+                match('*');
+                match('(');
+                exprlist();
+                match(')');
+                code.emit(OpCode.imul);
+                break;
+
+            case Tag.NUM:
+                match(Tag.NUM);
+                NumberTok x = (NumberTok)look;
+                int num_value = x.number;
+                code.emit(OpCode.iload, num_value);
+                break;
+    
+                
+    
+            case Tag.ID:
+                match(Tag.ID);
+                /*idlist(look.toString());*/
+                idlist();
+                /*Word a = (Word)look;
+                String c = a.lexeme;
+                code.emit(OpCode.iload, c);*/
+                break;
+            
+            default:
+            error("error in grammar <expr>");
         }
     }
 
-// ... completare ...
+    public static void main(String[] args) {
+        Lexer lex = new Lexer();
+        String path = "test.lft";
+        //String path = "max_tre_num.lft";
+        //String path = "factorial.lft";
+        //String path = "euclid.lft";
+        //String path = "esempio_semplice.lft";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            Translator traduttore = new Translator(lex, br);
+            traduttore.prog();
+            System.out.println("Translation success!");
+            br.close();
+        } catch (IOException e) {e.printStackTrace();}
+    }
 }
 
